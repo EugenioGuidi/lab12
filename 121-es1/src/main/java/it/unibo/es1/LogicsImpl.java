027987b0bfb8b5fb01b9a634354efcc591b64a55
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2024-12-09 11:54:18 +0100

    in exercise 121 implemented all methods of class LogicsImpl

--- 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
+++ 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
@@ -1,46 +1,71 @@
 package it.unibo.es1;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LogicsImpl implements Logics {
 
-	public LogicsImpl(int size) {
-		//TODO Auto-generated constructor stub
+	private int size;
+	private final List<Integer> listInteger;
+	private final List<Boolean> listBoolean;
+
+	public LogicsImpl(final int size) {
+		this.size = size;
+		this.listInteger = new ArrayList<>();
+		this.listBoolean = new ArrayList<>();
+		for (int i = 0; i < size; i++) {
+			listBoolean.add(true);
+			listInteger.add(0);
+		}
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'size'");
+		return this.size;
 	}
 
 	@Override
 	public List<Integer> values() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'values'");
+		return this.listInteger;
 	}
 
 	@Override
 	public List<Boolean> enablings() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'enablings'");
+		return this.listBoolean;
 	}
 
 	@Override
 	public int hit(int elem) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'hit'");
+		if (this.listBoolean.get(elem)) {
+			this.listInteger.set(elem, this.listInteger.get(elem) + 1);
+		}
+		if (this.listInteger.get(elem) == this.size) {
+			this.listBoolean.set(elem, false);
+		}
+		return this.listInteger.get(elem);
 	}
 
 	@Override
 	public String result() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'result'");
+		String string = "<<";
+		for(int i = 0; i < size; i++) {
+			string = string + listInteger.get(i);
+			if (i != size - 1) {
+				string = string + "|";
+			}
+		}
+		string = string + ">>";
+		return string;
 	}
 
 	@Override
 	public boolean toQuit() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'toQuit'");
+		boolean flag = true;
+		for(Integer integer : this.listInteger) {
+			if(!listInteger.get(0).equals(integer)) {
+				flag = false;
+			}
+		}
+		return flag;
 	}
 }
