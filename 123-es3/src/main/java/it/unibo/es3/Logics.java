b381ec4bd09eec63ffb9fc621936c5ad9de22781
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2024-12-14 17:26:19 +0100

    Restarted the exercise 123 to default implementation

--- 123-es3/src/main/java/it/unibo/es3/Logics.java
+++ 123-es3/src/main/java/it/unibo/es3/Logics.java
@@ -1,21 +1,5 @@
 package it.unibo.es3;
 
 public interface Logics {
-
-    /**
-     * 
-     * @param pair
-     * 
-     * @return @return the new value a button should show after being pressed
-     */
-    int[][] hit ();
-
-    public int[][] getMatrix();
-
-    /**
-     * 
-     * @return whether it is time to quit
-     */
-    boolean toQuit();
     
 }
