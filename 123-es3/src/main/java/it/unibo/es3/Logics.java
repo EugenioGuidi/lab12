58ac855a12f301b12f498ece8d091ae5f84b39fa
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2024-12-15 12:53:03 +0100

    Implemented all the exercise 123

--- 123-es3/src/main/java/it/unibo/es3/Logics.java
+++ 123-es3/src/main/java/it/unibo/es3/Logics.java
@@ -1,5 +1,13 @@
 package it.unibo.es3;
 
+import java.util.Set;
+
 public interface Logics {
+
+    void hit();
+
+    boolean toQuit();
+
+    Set<Pair<Integer, Integer>> getSet();
     
 }
--- 123-es3/src/main/java/it/unibo/es3/LogicsImpl.java
+++ 123-es3/src/main/java/it/unibo/es3/LogicsImpl.java
@@ -1,5 +1,52 @@
 package it.unibo.es3;
 
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 public class LogicsImpl implements Logics{
+
+    private final Set<Pair<Integer, Integer>> setTotal;
+    private final Set<Pair<Integer, Integer>> setNear;
+    private final int width;
+
+    public LogicsImpl(final int width) {
+        Random randomInt = new Random();
+        this.width = width;
+        this.setNear = new HashSet<>();
+        this.setTotal = new HashSet<>();
+        while(this.setTotal.size() < 3) {
+            this.setTotal.add(new Pair<Integer,Integer>(randomInt.nextInt(this.width), randomInt.nextInt(this.width)));
+        }
+    }
+
+    @Override
+    public void hit() {
+        this.setNear.clear();
+        for(int i = 0; i < this.width; i++) {
+            for(int j = 0; j < this.width; j++) {
+                Pair<Integer, Integer> pair = new Pair<>(i, j);
+                if(setTotal.stream().anyMatch(p -> near(p, pair))) {
+                    this.setNear.add(pair);
+                }
+            }
+        }
+        this.setTotal.addAll(this.setNear);
+    }
+
+    @Override
+    public boolean toQuit() {
+        return this.setTotal.size() == this.width * this.width;
+    }
+
+    public Set<Pair<Integer, Integer>> getSet() {
+        return new HashSet<>(this.setTotal);
+    }
+
+    private boolean near(Pair<Integer, Integer> firstPair, Pair<Integer, Integer> secondPair) {
+        boolean condition;
+        condition = (Math.abs(firstPair.getX() - secondPair.getX()) < 2 && Math.abs(firstPair.getY() - secondPair.getY()) < 2);
+        return condition;
+    }
     
 }
